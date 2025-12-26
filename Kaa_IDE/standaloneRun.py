eac8300baca892f8faaeeddc31750f0e20e37348
Leonid <leonid.Borolis@icloud.com>
2025-12-26 14:40:34 +0500

    Update standaloneRun.py
    Widget correct to stand alone

--- Kaa_IDE/standaloneRun.py
+++ Kaa_IDE/standaloneRun.py
@@ -6,7 +6,7 @@ import Kaa_IDE.UI.MainUI.mainWindow as editor
 def standalone_run():
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    win = editor.MainWindow()
+    win = editor.MainButton()
     win.show()
     sys.exit(app.exec())
 
