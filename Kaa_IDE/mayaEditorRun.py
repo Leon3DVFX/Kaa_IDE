1da9ded1db8ee4be3672ef66c9f869a94a53ed89
Leonid <leonid.Borolis@icloud.com>
2025-12-26 14:40:06 +0500

    Update mayaEditorRun.py
    Destroy obj on close

--- Kaa_IDE/mayaEditorRun.py
+++ Kaa_IDE/mayaEditorRun.py
@@ -5,8 +5,12 @@ def run():
     global _script_editor_button
     if _script_editor_button is None:
         _script_editor_button = MainButton()
-    _script_editor_button.show()
-    _script_editor_button.raise_()
+        _script_editor_button.show()
+        _script_editor_button.raise_()
+        _script_editor_button.destroyed.connect(_on_destr)
     return _script_editor_button
 
+def _on_destr(obj = None):
+    global _script_editor_button
+    _script_editor_button = None
 # Активация соотв. API
