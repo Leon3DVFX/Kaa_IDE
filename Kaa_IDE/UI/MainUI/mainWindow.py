8a7575ec99448f357bc56e769da5fff8beeabf67
Leonid <leonid.Borolis@icloud.com>
2026-01-07 19:33:36 +0500

    Update mainWindow.py
    icon change

--- Kaa_IDE/UI/MainUI/mainWindow.py
+++ Kaa_IDE/UI/MainUI/mainWindow.py
@@ -969,7 +969,7 @@ class MainWindow(QtWidgets.QWidget):
             elif kind == "methods":
                 elem1.setIcon(self.editor.complitter.base_model.b_icon)
             elif kind == "class":
-                elem1.setIcon(self.editor.complitter.base_model.class_icon)
+                elem1.setIcon(self.editor.complitter.base_model.c_class_icon)
                 elem2.setText(kind)
 
             self.editor.complitter.base_model.appendRow([elem1, elem2])
