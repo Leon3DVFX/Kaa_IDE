ef70b99a4e78bf7cd329f4ae7e7aed04d5a779f7
Leonid <leonid.Borolis@icloud.com>
2026-01-09 19:25:43 +0500

    Заготовка тестовых HTML и CSS для Help

--- Docs/Help/help.py
+++ Docs/Help/help.py
@@ -1,10 +1,19 @@
-from PySide6 import QtWidgets,QtCore, QtGui
+from PySide6 import QtWidgets, QtCore, QtWebEngineWidgets
+import os
 
-class HelpViewer(QtWidgets.QTextBrowser):
+
+class HelpViewer(QtWebEngineWidgets.QWebEngineView):
     def __init__(self, parent = None):
         super().__init__(parent)
-        self.setSource(QtCore.QUrl(r'help_en\help_en.html'),
-                       type = QtGui.QTextDocument.ResourceType.HtmlResource)
+        # Удаление при закрытии
+        self.setAttribute(QtCore.Qt.WidgetAttribute.WA_DeleteOnClose)
+
+        # Убрать контекстное меню
+        self.setContextMenuPolicy(QtCore.Qt.ContextMenuPolicy.NoContextMenu)
+
+        self.en_path = os.path.abspath(os.path.join('help_en', 'help_en.html'))
+        self.ru_path = os.path.abspath(os.path.join('help_ru', 'help_ru.html'))
+        self.setUrl(QtCore.QUrl.fromLocalFile(self.ru_path))
 
 
 if __name__ == '__main__':
