57dccd6a6a1aa0c722a6620625cd5689b308f90c
Leonid <leonid.Borolis@icloud.com>
2026-01-09 14:56:08 +0500

    dcc functions module create

---
+++ Core/dcc_functions.py
@@ -0,0 +1,23 @@
+
+# Проверки на соответствующие API
+# Autodesk Maya
+def is_maya():
+    try:
+        import maya.cmds
+        return True
+    except ImportError:
+        return False
+# Side FX Houdini
+def is_houdini():
+    try:
+        import hou
+        return True
+    except ImportError:
+        return False
+# Autodesk 3dsMax
+def is_max():
+    try:
+        import pymxs
+        return True
+    except ImportError:
+        return False
